6ae28f1bd34bd179e1c368ffa65e61d81a119848
andre_lacommare <alacommaresb@gmail.com>
2022-12-08 15:35:53 -0800

    Printing Multiple Variables with Personal Info

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main(String[] args){
+		String name = "Andre La Commare";
+		int age = 24;
+		String state = "California";
+		  System.out.println(name);
+		  System.out.println(age);
+		  System.out.println(state);
+	}
+}
