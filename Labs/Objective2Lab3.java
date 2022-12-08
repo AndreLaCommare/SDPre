b4cedff4539ff2c20e61431906bec8a1bf316bcb
andre_lacommare <alacommaresb@gmail.com>
2022-12-08 15:46:45 -0800

    Declaring Variables with Birthday Info

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args){
+		int birthYear = 1997;
+		String birthMonth = "December";
+		boolean isTodayAWeekend = false;
+		  System.out.println(birthYear);
+		  System.out.println(birthMonth);
+		  System.out.println(isTodayAWeekend);
+
+	}
+}
