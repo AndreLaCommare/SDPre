8747078da96232bb8323c1c2b06367a3f52cd4ae
andre_lacommare <alacommaresb@gmail.com>
2022-12-08 16:43:15 -0800

    Right Triangle - Finding the Hypotenuse

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+	public static void main(String[] args){
+		double side1 = 8;
+		double side2 = 5;
+		double hypotenuse = Math.sqrt((side1 * side1)+(side2 * side2));
+		System.out.println(hypotenuse);
+	}
+
+}
