488b2a34e76b6f8d4612c53d54c482c31a283ae5
andre_lacommare <alacommaresb@gmail.com>
2022-12-08 16:35:25 -0800

    Writing Expressions - Finding the Average

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+	public static void main(String[] args){
+		double num1 = 32;
+		double num2 = 4.65;
+		double num3 = 1111111.3;
+		double sum = num1 + num2 + num3;
+		double average = sum/3;
+		System.out.println(average);
+
+	}
+}
